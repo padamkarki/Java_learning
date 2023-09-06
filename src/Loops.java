57937012bacb489e6426066fde45404d333e2973
Padam Karki <padamkarkipkkp@gmail.com>
2023-09-06 13:13:45 +0530

    while, do-while loops

---
+++ src/Loops.java
@@ -0,0 +1,27 @@
+public class Loops {
+    public static void main(String[] args) {
+        //while loop
+        System.out.println("While Loop");
+        int num = 5;
+        while(num<=10){
+            System.out.println(num);
+            num++;
+        }
+
+        //do - while loop
+        System.out.println("Do-While Loop");
+        int i = 10;
+        do{
+            System.out.println(i);
+            i++;
+        }while(i<9);
+
+        //naturam numbers upto 50
+        System.out.println("Natural");
+        int natural = 1;
+        while(natural<=20){
+            System.out.println(natural);
+            natural++;
+        }
+    }
+}
