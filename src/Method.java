f6a75e714811d2ab3d0bc53d7271e3badcca8c3e
Padam Karki <padamkarkipkkp@gmail.com>
2023-09-06 22:08:57 +0530

    methods on array

--- src/Method.java
+++ src/Method.java
@@ -1,4 +1,19 @@
 public class Method {
+        //Method
+    static int logic(int x, int y){
+        int z;
+        if(x>y){
+            z= x/y;
+        }else{ 
+            z=x*y;
+        }
+        return z;
+    }
+    //for array/obj method is referencing to obj/array, so it will update
+    static void change(int [] arr){
+        arr[0] = 95;
+    }
+
     public static void main(String[] args) {
         int a = 24;
         int b = 32;
@@ -9,15 +24,11 @@ public class Method {
         int b1 = 32;
         int c1 = logic(a1, b1);
         System.out.println(c1);
+
+        //array method call
+        int [] marks = {12,13,54,46,66};
+        change(marks);
+        System.out.println("Updated 0th index: " + marks[0]);
     }
-        //Method
-        static int logic(int x, int y){
-        int z;
-        if(x>y){
-            z= x/y;
-        }else{
-            z=x*y;
-        }
-        return z;
-    }
+
 }
